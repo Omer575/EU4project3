69df9a941b92880da05d779152c65956622256da
Omer575 <omer.ulanuluu@hotmail.com>
2021-02-13 20:32:33 +0100

    working on message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
         System.out.println("Hello Git Last Day");
+        // EU4 is exited !!!
     }
 }
